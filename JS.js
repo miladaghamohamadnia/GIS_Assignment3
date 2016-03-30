b033683b8508792855984616c4f404538ab17e48
Milad Aghamohamadnia <ma3725@nyu.edu>
2016-03-30 04:38:12 -0400

    First draft

---
+++ JS.js
@@ -0,0 +1,12 @@
+$('#ToBlue').click(function() {
+  $('#Paragraph').css('color', 'blue');
+});
+$('#ToRed').click(function() {
+  $("#Paragraph").css('color', 'red')
+});
+$('#ToGreen').click(function() {
+  $("#Paragraph").css('color', 'green')
+});
+$('#ToDefault').click(function() {
+  $("#Paragraph").css('color', 'white')
+});
